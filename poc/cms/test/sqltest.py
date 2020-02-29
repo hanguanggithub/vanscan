4bd09d11c31f0b43154e2c973914fc6473d21e30
shadow1ng <819431368@qq.com>
2020-02-29 15:24:59 +0800

    'test'

---
+++ poc/cms/test/sqltest.py
@@ -0,0 +1,26 @@
+# ! usr/bin/env python
+#  -*- coding: utf-8 -*-
+# @Author  : Y4er
+# @File    : sqltest.py
+import requests
+
+
+class sqltest:
+    def __init__(self, url):
+        self.url = url
+
+    def run(self):
+        url = self.url
+        data = "'"
+        try:
+            req1 = requests.get(url,timeout=10).headers['Content-Length']
+            print(1111111111111,req1)
+            req2 = requests.get(url + data,timeout=10).headers['Content-Length']
+            print(2222222222222,req2)
+            if req1 != req2:
+                return '[+] 存在注入'
+            else:
+                return '[-]不存在注入'
+        except Exception as e:
+            print(e)
+            return '[-]访问异常'
